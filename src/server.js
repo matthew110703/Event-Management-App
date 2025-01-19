afe20335c03cb630130dbafed4cbd36480591b56
R Mathew <140831930+matthew110703@users.noreply.github.com>
2025-01-19 15:08:00 +0530

    Update console.log()

--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ import app from "./app.js";
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
-  console.log("Server is running on http://localhost:3000");
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
