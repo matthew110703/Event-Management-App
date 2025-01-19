8e489807f64747a9e5f26bd7ace78dfe6faccaa0
R Mathew <140831930+matthew110703@users.noreply.github.com>
2025-01-19 16:14:23 +0530

    Enhanced error response

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,14 +1,14 @@
 /** @description Error Handler Middlware */
 const errorHandler = (err, req, res, next) => {
-  // Status code
-  const statusCode = err.StatusCode || 500;
+  // Status code and details
+  const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
 
   // Log
   console.log(`Error: ${err.message}`, err);
 
   // Response
-  res.status(statusCode).json({ error: message });
+  res.status(statusCode).json({ errorCode: statusCode, error: message });
 };
 
 export default errorHandler;
