f1878b8e815dc3f34c0611ef31ed93459c63ffc8
R Mathew <140831930+matthew110703@users.noreply.github.com>
2025-01-19 14:50:34 +0530

    Added Morgan Logger

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
 import express from "express";
+import morgan from "morgan";
 
-const app = express();
+const app = express(); // App
 
+// Middlewares
+app.use(morgan("dev"));
+app.use(express.json());
+
+// Routes
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
