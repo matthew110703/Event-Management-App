3ddf941dbea5aae4b5ca7038fbda9f01be438b17
R Mathew <140831930+matthew110703@users.noreply.github.com>
2025-01-19 15:14:37 +0530

    Initialized Database Connection

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
 import app from "./app.js";
 import dotenv from "dotenv";
+import dbConnect from "./lib/dbConnect.js";
+
 // Load Environment Variables
 dotenv.config();
+
+// Database Connection
+dbConnect();
 
 const PORT = process.env.PORT || 3001;
 
