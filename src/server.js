9eda08af63d3ab0c2b7138f4636c4f4481ba8dc2
R Mathew <140831930+matthew110703@users.noreply.github.com>
2025-01-20 22:10:14 +0530

    Initialized httpServer and Socket.io Connection

--- src/server.js
+++ src/server.js
@@ -1,4 +1,6 @@
 import app from "./app.js";
+import { createServer } from "http";
+import { Server } from "socket.io";
 import dotenv from "dotenv";
 import dbConnect from "./lib/dbConnect.js";
 
@@ -8,8 +10,24 @@ dotenv.config();
 // Database Connection
 dbConnect();
 
+// HTTP Server Initialization
+const httpServer = createServer(app);
+
+// Socket.io Connection
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*",
+  },
+  pingTimeout: 60000,
+  pingInterval: 25000,
+});
+
+// WebSockets Event Updates
+import { eventUpdates } from "./controllers/eventUpdateController.js";
+eventUpdates(io);
+
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+httpServer.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
