5086c5174f0fc4c1857d3f5c727d3d87dde965bc
R Mathew <140831930+matthew110703@users.noreply.github.com>
2025-01-21 11:26:55 +0530

    Initialized pages structure and routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+// Pages
+import { Login, Register, Dashboard, EventDetail, NotFound } from "./pages";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/event/:id" element={<EventDetail />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import Login from "./Login";
+import Register from "./Register";
+import Dashboard from "./Dashboard";
+import EventDetail from "./EventDetail";
+import NotFound from "./NotFound";
+
+export { Login, Register, Dashboard, EventDetail, NotFound };
