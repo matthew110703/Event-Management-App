05e7edb6a758678f28f693be386b06808d6ff373
R Mathew <140831930+matthew110703@users.noreply.github.com>
2025-01-21 12:52:59 +0530

    Create Navigation Component

---
+++ src/components/layout/Navbar.jsx
@@ -0,0 +1,63 @@
+import { Link } from "react-router-dom";
+
+import { logo, heartIcon, menuIcon, homeIcon } from "../../assets";
+import NavLink from "../ui/NavLink";
+
+const Navbar = () => {
+  return (
+    <header className="navbar rounded-md shadow-lg">
+      <div className="navbar-start">
+        <Link
+          to="/"
+          className="flex items-center gap-2 text-xl font-bold text-info"
+        >
+          <img src={logo} alt="logo" width={32} />
+          Eventify
+        </Link>
+      </div>
+
+      <div className="navbar-end hidden gap-6 text-sm *:font-semibold md:flex">
+        <NavLink icon={homeIcon} title="Home" />
+        <NavLink icon={heartIcon} title="My Events" />
+        <Link
+          to="/login"
+          className="navbar-item btn btn-info btn-sm text-white"
+        >
+          Logout
+        </Link>
+      </div>
+
+      {/* Dropdown menu for mobile */}
+      <div className="navbar-end flex flex-none gap-4 md:hidden">
+        <div className="dropdown dropdown-left">
+          <button tabIndex={0} className="btn btn-square btn-sm">
+            <img src={menuIcon} alt="menu" width={24} />
+          </button>
+
+          <ul
+            tabIndex={0}
+            className="dropdown-content z-[1] mt-14 w-52 space-y-2 text-nowrap rounded-lg bg-white px-4 py-2 shadow *:text-sm"
+          >
+            <li>
+              <NavLink icon={homeIcon} title="Home" />
+            </li>
+
+            <li>
+              <NavLink icon={heartIcon} title="My Events" />
+            </li>
+            <li>
+              <Link
+                to="/login"
+                className="navbar-item btn btn-info btn-sm mx-auto w-full text-white"
+              >
+                Logout
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
---
+++ src/components/ui/NavLink.jsx
@@ -0,0 +1,15 @@
+import { Link } from "react-router-dom";
+
+const NavLink = ({ title, icon, className, href = "#" }) => {
+  return (
+    <Link
+      to={href}
+      className={`navbar-item flex items-center gap-1.5 rounded-lg p-2 text-sm hover:text-info hover:shadow ${className}`}
+    >
+      {icon && <img src={icon} alt="heart" width={18} />}
+      {title}
+    </Link>
+  );
+};
+
+export default NavLink;
