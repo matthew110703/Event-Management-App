41909a59bb8faa602c679425dbc77d19c588de9f
R Mathew <140831930+matthew110703@users.noreply.github.com>
2025-01-21 12:52:00 +0530

    Added svg icons

---
+++ src/assets/index.js
@@ -0,0 +1,6 @@
+import logo from "./icons/logo.svg";
+import menuIcon from "./icons/menu.svg";
+import heartIcon from "./icons/heart.svg";
+import homeIcon from "./icons/home.svg";
+
+export { logo, menuIcon, heartIcon, homeIcon };
