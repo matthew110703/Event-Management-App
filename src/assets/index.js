0a20d111c58258b7203fadac6ff8bf0b513534d8
R Mathew <140831930+matthew110703@users.noreply.github.com>
2025-01-21 16:25:48 +0530

    Added new assets

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,5 +2,7 @@ import logo from "./icons/logo.svg";
 import menuIcon from "./icons/menu.svg";
 import heartIcon from "./icons/heart.svg";
 import homeIcon from "./icons/home.svg";
+import profileIcon from "./icons/profile.svg";
+import banner from "./banner.jpg";
 
-export { logo, menuIcon, heartIcon, homeIcon };
+export { logo, menuIcon, heartIcon, homeIcon, profileIcon, banner };
