d2f6683a90a8c4bcfdde3ab669d937d9dfe3fcfb
R Mathew <140831930+matthew110703@users.noreply.github.com>
2025-01-22 12:56:40 +0530

    Added new page 'MyEvents'

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,8 @@
 import Login from "./Login";
 import Register from "./Register";
 import Dashboard from "./Dashboard";
+import MyEvents from "./MyEvents";
 import EventDetail from "./EventDetail";
 import NotFound from "./NotFound";
 
-export { Login, Register, Dashboard, EventDetail, NotFound };
+export { Login, Register, Dashboard, MyEvents, EventDetail, NotFound };
