3fb10f24ae83d696d364f66e8194b202849a1d82
R Mathew <140831930+matthew110703@users.noreply.github.com>
2025-01-22 12:53:10 +0530

    Update NavLink.jsx

--- src/components/ui/NavLink.jsx
+++ src/components/ui/NavLink.jsx
@@ -4,7 +4,7 @@ const NavLink = ({ title, icon, className, href = "#" }) => {
   return (
     <Link
       to={href}
-      className={`navbar-item flex items-center gap-1.5 rounded-lg p-2 text-sm hover:text-info hover:shadow ${className}`}
+      className={`navbar-item flex items-center gap-1.5 whitespace-nowrap rounded-lg p-2 text-sm hover:text-info hover:shadow ${className}`}
     >
       {icon && <img src={icon} alt="heart" width={18} />}
       {title}
