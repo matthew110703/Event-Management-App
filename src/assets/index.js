0fa46a010a4042c259f5ee8fcb15e9cc9b78b0ce
R Mathew <140831930+matthew110703@users.noreply.github.com>
2025-01-22 12:28:10 +0530

    Added new icons

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,5 +4,30 @@ import heartIcon from "./icons/heart.svg";
 import homeIcon from "./icons/home.svg";
 import profileIcon from "./icons/profile.svg";
 import banner from "./banner.jpg";
+import usersIcon from "./icons/users.svg";
+import adminIcon from "./icons/admin.svg";
+import groupIcon from "./icons/group.svg";
+import geoIcon from "./icons/location.svg";
+import dateIcon from "./icons/date.svg";
+import privateIcon from "./icons/private.svg";
+import filterIcon from "./icons/filter.svg";
+import closeIcon from "./icons/close.svg";
+import clearIcon from "./icons/clear.svg";
 
-export { logo, menuIcon, heartIcon, homeIcon, profileIcon, banner };
+export {
+  logo,
+  menuIcon,
+  heartIcon,
+  homeIcon,
+  profileIcon,
+  banner,
+  usersIcon,
+  adminIcon,
+  groupIcon,
+  geoIcon,
+  dateIcon,
+  privateIcon,
+  filterIcon,
+  closeIcon,
+  clearIcon,
+};
