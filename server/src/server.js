9fb7813879eb498e4978646414eb62c852dde307
R Mathew <140831930+matthew110703@users.noreply.github.com>
2025-01-27 15:20:06 +0530

    Removed socket config and added CORS headers

--- server/src/server.js
+++ server/src/server.js
@@ -1,33 +1,19 @@
 import app from "./app.js";
-import { createServer } from "http";
-import { Server } from "socket.io";
 import dotenv from "dotenv";
 import dbConnect from "./lib/dbConnect.js";
+import cors from "cors";
 
 // Load Environment Variables
 dotenv.config();
 
+// CORS
+app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173" }));
+
 // Database Connection
 dbConnect();
 
-// HTTP Server Initialization
-const httpServer = createServer(app);
-
-// Socket.io Connection
-const io = new Server(httpServer, {
-  cors: {
-    origin: "*",
-  },
-  pingTimeout: 60000,
-  pingInterval: 25000,
-});
-
-// WebSockets Event Updates
-import { eventUpdates } from "./controllers/eventUpdateController.js";
-eventUpdates(io);
-
 const PORT = process.env.PORT || 3001;
 
-httpServer.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
